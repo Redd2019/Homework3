6c5ac6f3dc5cc3ad7a23b9750bea821f2b251fdf
Redd2019` <anton-mikerin@yandex.ru>
2023-01-13 23:17:02 +0300

    Выполнено задание 1

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        task1 ();
+
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int sugar = 34508;
+        byte salt = 121;
+        short pepper = 323;
+        long rice = 1500000;
+        float apples = 3.14F;
+        double cherries = 5.764527882645782;
+
+        System.out.println("Значение переменной sugar с типом int равно " + sugar);
+        System.out.println("Значение переменной salt с типом byte равно " + salt);
+        System.out.println("Значение переменной pepper с типом short равно " + pepper);
+        System.out.println("Значение переменной rice с типом long равно " + rice);
+        System.out.println("Значение переменной apples с типом float равно " + apples);
+        System.out.println("Значение переменной cherries с типом double равно " + cherries);
+    }
+}
